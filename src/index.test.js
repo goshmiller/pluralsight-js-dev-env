63ed2c9aa77fa6517cf0690a749765d9bf0ec922
Josh Miller <goshmiller@gmail.com>
2017-06-04 10:41:38 -0400

    Nearly complete.

--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,11 @@ describe('The first test', () => {
 } );
 
 describe('index.html', () => {
-  it('should say greetings', (done) => {
+  it('should have H1 that says Users', (done) => {
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function(err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal("Greetings Human.");
+      expect(h1.innerHTML).to.equal("Users");
       window.close();
       done();
     });
